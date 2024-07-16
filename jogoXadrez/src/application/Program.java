3bb3dbabc0b1a26968b9fece2b42a16ca053df6b
marcus aurelio <marcusaurelio743@gmail.com>
2024-07-16 18:02:40 -0300

    criando peça de chadrez e imprimindo tabuleiro com as peças

--- jogoXadrez/src/application/Program.java
+++ jogoXadrez/src/application/Program.java
@@ -1,14 +1,14 @@
 package application;
 
-import boardgame.Board;
-import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		
-		Board board = new Board(4, 3);
+		ChessMatch match = new ChessMatch();
+		UI.printBoard(match.getPieces());
 	}
 
 }
