4ee11e8519eb13c81f8a84c24c32380f29af4a00
marcus aurelio <marcusaurelio743@gmail.com>
2024-11-04 16:37:35 -0300

    instalando movimento da peça torre para o jogo

--- jogoXadrez/src/chess/ChessMatch.java
+++ jogoXadrez/src/chess/ChessMatch.java
@@ -33,6 +33,7 @@ public class ChessMatch {
 		Position source = sourcePosition.toPosition();
 		Position target = targetPosition.toPosition();
 		validateSourcePosition (source);
+		ValidateTargetPosition(source,target);
 		Piece capturedPiece = makeMove(source,target);
 		
 		return (ChessPiece)capturedPiece;
@@ -45,6 +46,11 @@ public class ChessMatch {
 		}
 		if(!board.piece(position).isThereAnyPossibleMove()) {
 			throw new ChessException("there is no possible moves for the chosen piece ");
+		}
+	}
+	private void ValidateTargetPosition(Position source,Position target) {
+		if(! board.piece(source).possibleMove(target)) {
+			throw new ChessException("the chosen piece can't move to target position ");
 		}
 	}
 	
--- jogoXadrez/src/chess/pieces/Rook.java
+++ jogoXadrez/src/chess/pieces/Rook.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -19,6 +20,59 @@ public class Rook extends ChessPiece {
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+			
+			//ter uma posição auxiliar
+			Position p = new Position(0, 0);
+			
+			//verificar posição acima 
+			p.setValues(position.getRow() - 1 , position.getColumn());
+			
+			while(getBoard().PositionExists(p) && !getBoard().TheresIsApieces(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+				p.setRow(p.getRow() - 1);
+			}
+			
+			if(getBoard().PositionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			
+			//verificar posição esquerda 
+			p.setValues(position.getRow()  , position.getColumn() - 1);
+			
+			while(getBoard().PositionExists(p) && !getBoard().TheresIsApieces(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+				p.setColumn(p.getColumn() - 1);
+			}
+			
+			if(getBoard().PositionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			
+			//verificar posição direita 
+			p.setValues(position.getRow()  , position.getColumn() + 1);
+			
+			while(getBoard().PositionExists(p) && !getBoard().TheresIsApieces(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+				p.setColumn(p.getColumn() + 1);
+			}
+			
+			if(getBoard().PositionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			
+			//verificar posição baixo 
+			p.setValues(position.getRow() + 1 , position.getColumn());
+			
+			while(getBoard().PositionExists(p) && !getBoard().TheresIsApieces(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+				p.setRow(p.getRow() + 1);
+			}
+			
+			if(getBoard().PositionExists(p) && isThereOpponentPiece(p)) {
+				mat[p.getRow()][p.getColumn()] = true;
+			}
+			
+			
 		return mat;
 	}
 	
